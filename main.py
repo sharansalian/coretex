5c81c14b78e8e87d7d5726390a1cda73c1d133c5
sharansalian <sharansalian.dev@gmail.com>
2022-11-12 10:08:18 +0530

    Update graph practice code

--- main.py
+++ main.py
@@ -3,14 +3,21 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+def max_sum():
+    for i in range(0, 6):
+        print("i =", i)
+        for j in range(i, 6):
+            print("j = ", j)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def directions():
+    return [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    for dr, dc in directions():
+        print("dr =", dr, dc)
+    print(directions())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
