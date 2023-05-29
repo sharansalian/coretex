b95aabfea05f56972192b515461555ea987bb073
sharansalian <sharansalian.dev@gmail.com>
2023-05-29 07:46:19 +0530

    Add parking_lot

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 # This is a sample Python script.
+import collections
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-def max_sum():
-    for i in range(0, 6):
-        print("i =", i)
-        for j in range(i, 6):
-            print("j = ", j)
+def bfs(grid):
+    ROWS, COLS = len(grid), len(grid[0])
+    visit = set()
 
+    queue = collections.deque()
+    queue.append((0, 0))
 
-def directions():
-    return [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    length = 0
+
+    while queue:
+        for i in range(len(queue)):
+            r, c = queue.popleft()
+            if r == ROWS - 1 and c == COLS - 1:
+                return length
+
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for dr, dc in directions:
+                row, col = r + dr, c + dc
+                if (min(row, col) < 0
+                        or row == ROWS
+                        or col == COLS
+                        or (row, col) in visit
+                        or grid[row][col] == 1):
+                    continue
+
+                visit.add((row, col))
+                queue.append((row, col))
+        length += 1
 
 
 # Press the green button in the gutter to run the script.
+# Find the shortest path from top left to bottom righ point
+
 if __name__ == '__main__':
-    for dr, dc in directions():
-        print("dr =", dr, dc)
-    print(directions())
+    grid = [[0, 0, 0, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 1],
+            [0, 1, 0, 0]]
+
+    print(bfs(grid))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
